b0cad8b376786ea38bec72e3293360b91114702c
Ramon Williams <ramnwor21@gmail.com>
2022-09-21 15:04:41 +0200

    Prueba de subida

---
+++ app/src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,44 @@
+import { API } from '../../services/API';
+import { useParams } from 'react-router-dom';
+import "./Favorites.css"
+import React, { useEffect, useState, useContext } from 'react';
+import { JwtContext } from '../../context/jwtContext';
+
+import ExperienceCard from '../../components/ExperienceCard/ExperienceCard';
+const Favorites = () => {
+    const [allFavorites, setAllFavorites] = useState([]);
+    const {user} = useContext(JwtContext)
+    const { username } = useParams();
+console.log(user, username)
+
+    const getAllFavorites = async () => {
+      API.get(`/${user}/${username}`).then((res) => {
+          console.log("hOLA",res)
+        setAllFavorites(res.data.data.favoriteExperience);      
+      });
+    }
+
+    const getFavorites = () => {
+     const userStorage = JSON.parse(localStorage.getItem("user"))
+     const favorites = userStorage.favoriteExperience;
+     setAllFavorites(favorites)
+    };
+  
+    useEffect(() => {
+      getAllFavorites()
+     
+     
+    }, []);
+   return(
+
+    <section className='favoritos'> 
+        {allFavorites.length ? allFavorites.map((favorite) => <ExperienceCard experience={favorite} key={favorite._id}/>) :<p></p>}
+        <h2>En este momento está la página en mantenimiento,disculpen las molestias</h2>
+        <img src='https://c.tenor.com/1qrYT711uEoAAAAC/cargando.gif'/>
+    </section>
+   
+   ) 
+    
+ 
+}
+export default Favorites
